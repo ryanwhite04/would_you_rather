e935630d3941376d3f5cb6e57df14dde243c008e
Ryan White <ryanwhite04@gmail.com>
2021-02-26 14:23:17 +0800

    Let admin view a question and stats

--- admin.py
+++ admin.py
@@ -54,7 +54,7 @@ def input_boolean(prompt):
 # See Point 3 of the "Functions in admin.py" section of the assignment brief.
 def save_data(data_list):
     with open("data.txt", "w") as output:
-        json.dump(data_list, output)
+        json.dump(data_list, output, indent=4)
 
 # Here is where you attempt to open data.txt and read the data into a "data" variable.
 # If the file does not exist or does not contain JSON data, set "data" to an empty list instead.
@@ -70,9 +70,10 @@ def load_data():
 
 def addQuestion():
     questions = load_data()
-    option1 = input_something("Enter first option: ")
+    print('Both options should be phrased to follow "Would you rather..."')
+    option1 = input_something("Enter option 1: ")
     while True:
-        option2 = input_something("Enter second option: ")
+        option2 = input_something("Enter option 2: ")
         if option2 == option1:
             print("The options should be different")
             continue
@@ -81,7 +82,7 @@ def addQuestion():
     question = {
         "option_1": option1[0:-1] if option1.endswith("?") else option1,
         "option_2": option2[0:-1] if option2.endswith("?") else option2,
-        "mature": input_boolean("Is this question for mature audiences only?: "),
+        "mature": input_boolean("Is this question intended for mature audiences only? [Y/N]: "),
         "votes_1": 0,
         "votes_2": 0,
     }
@@ -89,6 +90,7 @@ def addQuestion():
     save_data(questions)
 
 def listQuestions():
+    print("List of questions:")
     questions = load_data()
     if len(questions):
         for i, question in enumerate(questions):
@@ -103,16 +105,43 @@ def searchQuestions():
         query = term.lower()
         return query in option_1 or query in option_2
 
-    query = input_something("Enter a search term: ")
+    query = input_something("Enter search term: ")
     questions = load_data()
     matches = [question for question in enumerate(questions) if questionHasTerm(query, question)]
     if len(matches):
+        print("Search results:")
         for i, match in matches:
-            print(i+1, f'{match["option_1"]}/{match["option_2"]}') 
+            print(f' {i+1}) {match["option_1"]} / {match["option_2"]}') 
     else:
         print("No questions saved")
+
+def displayVotes(votes_1, votes_2):
+    if (votes_1 or votes_2):
+        ratio_1 = round(100*votes_1/(votes_1 + votes_2), 1)
+        ratio_2 = round(100 - ratio_1, 1)
+        noun_1 = "votes" if votes_1 != 1 else "vote"
+        noun_2 = "votes" if votes_2 != 1 else "vote"
+        print(f'Option 1 has received {votes_1} ({ratio_1}%) {noun_1}, Option 2 has received {votes_2} ({ratio_2}%) {noun_2}.') 
+    else:
+        print("Nobody has answered this question")
+
+def displayQuestion(question):
+    print("Would you rather...")
+    print(f' Option 1) {question["option_1"]}?')
+    print(f' Option 2) {question["option_2"]}?\n')
+    question['mature'] and print('This question is for mature audiences only')
+    displayVotes(question["votes_1"], question["votes_2"])
+
 def viewQuestion():
-    pass
+    questions = load_data()
+    if len(questions):
+        index = input_int("Enter the index of a question: ") - 1
+        if 0 <= index < len(questions):
+            displayQuestion(questions[index])
+        else:
+            print("Invalid index number")
+    else:
+        print("No questions saved")
 
 def deleteQuestion():
     pass
--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 from admin import *
 from json import dump, load
-# input_int("Type an integer: ")
-# input_something("Type anything but whitespace: ")
-# input_boolean("Type Yes or No")
+
+questions = [
+    {
+        "option_1": "Fight a horse-sized duck",
+        "option_2": "Fight 100 duck-sized horsed",
+        "mature": True,
+        "votes_1": 3,
+        "votes_2": 5,
+    },
+    {
+        "option_1": "Be invisible",
+        "option_2": "Be able to fly",
+        "mature": False,
+        "votes_1": 0,
+        "votes_2": 0,
+    }
+]
+save_data(questions)
+input_int("Type an integer: ")
+input_something("Type anything but whitespace: ")
+input_boolean("Type Yes or No")
 addQuestion()
 listQuestions()
 searchQuestions()
+viewQuestion()
